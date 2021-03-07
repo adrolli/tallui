d501cbeb552992e5d7910a6191e53820e65c8bfd
Alf <alf@drollinger.info>
2021-03-07 02:40:04 +0100

    Update LoginComponent.php

---
+++ src/Components/LoginComponent.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace TallUi\TallUi\Components;
+
+use Livewire\Component;
+
+class LoginComponent extends Component
+{
+
+    public function render()
+    {
+        return view('tallui::login-component');
+    }
+}
