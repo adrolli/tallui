398550ff77e613f567c27fd1ee56382c24edec8b
Alf Drollinger <40421928+adrolli@users.noreply.github.com>
2021-03-08 18:16:38 +0100

    Update Login.php

--- src/Components/Login.php
+++ src/Components/Login.php
@@ -3,9 +3,47 @@
 namespace TallUi\TallUi\Components;
 
 use Livewire\Component;
+use Illuminate\Support\Facades\Auth;
 
 class Login extends Component
 {
+
+    public $email;
+    public $password;
+
+    public function submitForm()
+    {
+
+        if (Auth::attempt(array('email' => $this->email, 'password' => $this->password))) {
+
+            session()->forget('login.attempts');
+            session()->regenerate();
+            return redirect()->intended('dashboard');
+
+        } else {
+
+            $attempts = session()->get('login.attempts', 0);
+            $attempts = $attempts + 1;
+            session()->put('login.attempts', $attempts);
+
+            $this->letWait($attempts);
+
+            session()->flash('error', 'User and password do not match.');
+
+        }
+
+    }
+
+    public function letWait($attempts)
+    {
+
+        if ($attempts % 2 == 0 && $attempts != 0) {
+
+            session()->flash('letwait', '4');
+
+        }
+
+    }
 
     public function render()
     {
